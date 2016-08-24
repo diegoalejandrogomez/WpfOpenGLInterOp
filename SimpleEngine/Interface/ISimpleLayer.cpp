22f4a4dfdad8607e889a422fc10033a228c8df24
Diego <diego.gomez2005@gmail.com>
2016-08-24 08:59:59 -0300

    partial commit, integration with ManagedSimpleLayer

--- SimpleEngine/Interface/ISimpleLayer.cpp
+++ SimpleEngine/Interface/ISimpleLayer.cpp
@@ -24,47 +24,47 @@ SIMPLE_API SimpleObject* SimpleLayer_EntitiesEnd(SimpleLayer* layer) {
 	return nullptr;
 }
 
-SIMPLE_API void AddEntity(SimpleLayer* sLayer, SimpleObject * sObj)
+SIMPLE_API void SimpleLayer_AddEntity(SimpleLayer* sLayer, SimpleObject * sObj)
 {
 	sLayer->AddEntity(sObj);
 }
 
-SIMPLE_API bool RemoveEntity(SimpleLayer* sLayer, SimpleObject * sObj)
+SIMPLE_API bool SimpleLayer_RemoveEntity(SimpleLayer* sLayer, SimpleObject * sObj)
 {
 	return sLayer->RemoveEntity(sObj);
 }
 
-SIMPLE_API void Clear(SimpleLayer* sLayer, bool deleteEntities)
+SIMPLE_API void SimpleLayer_Clear(SimpleLayer* sLayer, bool deleteEntities)
 {
 	return sLayer->Clear(deleteEntities);
 }
      
-SIMPLE_API void SetZ(SimpleLayer* sLayer, float Z)
+SIMPLE_API void SimpleLayer_SetZ(SimpleLayer* sLayer, float Z)
 {
 	sLayer->SetZ(Z);
 }
 
-SIMPLE_API float GetZ(SimpleLayer* sLayer)
+SIMPLE_API float SimpleLayer_GetZ(SimpleLayer* sLayer)
 {
 	return sLayer->GetZ();
 }
 
-inline SIMPLE_API const SimpleID & GetName(SimpleLayer* sLayer)
+inline SIMPLE_API const SimpleID & SimpleLayer_GetName(SimpleLayer* sLayer)
 {
 	return sLayer->GetName();
 }
 
-SIMPLE_API void SetName(SimpleLayer* sLayer, SimpleID name)
+SIMPLE_API void SimpleLayer_SetName(SimpleLayer* sLayer, SimpleID name)
 {
 	sLayer->SetName(name);
 }
 
-SIMPLE_API void SetQueryable(SimpleLayer* sLayer, bool value)
+SIMPLE_API void SimpleLayer_SetQueryable(SimpleLayer* sLayer, bool value)
 {
 	sLayer->SetQueryable(value);
 }
 
-SIMPLE_API bool IsQueryable(SimpleLayer* sLayer)
+SIMPLE_API bool SimpleLayer_IsQueryable(SimpleLayer* sLayer)
 {
 	return sLayer->IsQueryable();
 }
---
+++ SimpleEngineTileEditor/ManagedSimpleLayer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEngineTileEditor
+{
+    class ManagedSimpleLayer
+    {
+        #region DllImports
+        //Managed simple object imports
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern void SimpleLayer_AddEntity(IntPtr sLayer, IntPtr sObj);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern bool SimpleLayer_RemoveEntity(IntPtr sLayer, IntPtr sObj);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern void SimpleLayer_Clear(IntPtr sLayer, bool deleteEntities = false);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern void SimpleLayer_SetZ(IntPtr sLayer, float Z);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern float SimpleLayer_GetZ(IntPtr sLayer);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern String SimpleLayer_GetName(IntPtr sLayer);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern void SimpleLayer_SetName(IntPtr sLayer, String name);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern void SimpleLayer_SetQueryable(IntPtr sLayer, bool value);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern bool SimpleLayer_IsQueryable(IntPtr sLayer);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern String SimpleLayer_Serialize(IntPtr sLayer);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern bool SimpleLayer_Deserialize(IntPtr sLayer, String layer);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern IntPtr SimpleLayer_EntitiesBegin(IntPtr layer);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern IntPtr SimpleLayer_EntitiesNext(IntPtr layer);
+
+        [DllImport("SimpleEngine_dyn.dll")]
+        static extern IntPtr SimpleLayer_EntitiesEnd(IntPtr layer);
+        #endregion
+        IntPtr sLayer;
+
+        public void Remove()
+        {
+        }
+
+        public ManagedSimpleLayer(IntPtr simpleLayer)
+        {
+            sLayer = simpleLayer;
+        }
+
+        public float Z
+        {
+            get
+            {
+                return SimpleLayer_GetZ(sLayer);
+            }
+            set
+            {
+                SimpleLayer_SetZ(sLayer, value);
+            }
+        }
+
+        public String Name
+        {
+            get
+            {
+                return SimpleLayer_GetName(sLayer);
+            }
+            set
+            {
+                SimpleLayer_SetName(sLayer, value);
+            }
+        }
+
+        /*public List<ManagedSimpleObject> SimpleObjects
+        {
+            get
+            {
+                return SimpleLayer_GetSimpleObjects(sLayer)
+            }
+
+            set
+            {
+                SimpleLayer_SetSimpleObjects(sLayer, value);
+            }
+        }*/
+    }
+    
+}
