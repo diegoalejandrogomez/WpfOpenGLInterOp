e0648f5da3ab83258b606469b7eefdb0989501f7
sebastian.rojas <sebastian.rojas@AR-IT08104>
2016-09-08 10:20:38 -0300

    +Added SimpleController interface 1st ver

---
+++ SimpleEngine/Src/SimpleController.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "SimpleController.h"
+
+void SimpleController::Control(SimpleCharacter* character) {
+
+}
+void SimpleController::Release(SimpleCharacter* character) {
+
+
+}
+
+void SimpleController::Update(float dt) {
+
+
+}
